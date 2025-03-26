f9acbe7b23a09310b95adee5c1ff62629b466f66
Andre Luis <andre.1357.luis@gmail.com>
2025-03-26 20:52:18 -0300

    🔧 Reajusta eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,23 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import securityNode from 'eslint-plugin-security-node';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: {
+      globals: globals.node,
+    },
+    plugins: {
+      'security-node': securityNode,
+    },
+    // extends: ['standard-with-typescript', 'prettier'],
+    rules: {
+      ...securityNode.configs.recommended.rules,
+    },
+  },
   tseslint.configs.recommended,
+  eslintConfigPrettier,
 ]);
