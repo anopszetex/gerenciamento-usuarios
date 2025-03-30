88a585d433a255d0d540ce3c5d8f8a856301ecbf
Andre Luis <andre.1357.luis@gmail.com>
2025-03-30 16:11:13 -0300

    🥅 Destrói conexão ao encerrar servidor

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,14 +2,21 @@ import closeWithGrace from 'close-with-grace';
 
 import { factoryServer } from '@/adapters/server/fastify';
 
+import { createConnection } from '@/infra/db';
+import { loadDb } from '@/base/contextStrategy';
+
 import type { BuildServer } from './types';
 import type { Logger } from 'pino';
 
 function buildServer(logger: Logger): BuildServer {
-  const { listen, stop } = factoryServer();
+  const connection = createConnection(logger);
+  const database = loadDb(connection);
+
+  const { listen, stop } = factoryServer(database, logger);
 
   closeWithGrace({ delay: 500 }, async function () {
     logger.warn('aborting application');
+    await database.destroy();
     await stop();
   });
 
