16088e798b3f971276b0dbc2410eb73942b293b9
Andre Luis <andre.1357.luis@gmail.com>
2025-03-30 21:26:01 -0300

    🗃️ Atualiza o número de caracteres para salvar senha

--- src/infra/db/migrations/20250329190201_create_table_users.ts
+++ src/infra/db/migrations/20250329190201_create_table_users.ts
@@ -5,7 +5,7 @@ export async function up(knex: Knex): Promise<void> {
     table.increments('id').primary();
     table.string('nome', 50).notNullable();
     table.string('email', 50).notNullable().unique();
-    table.string('senha', 50).notNullable();
+    table.string('senha', 200).notNullable();
     table.string('cpf', 20).notNullable().unique();
   });
 }
